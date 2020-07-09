49b16422c2c8e66d2e2d393b916768afc5242988
Carlos <intercrios@hotmail.com>
2020-07-09 19:44:04 -0300

    PHP Rest Segundo Testeo PHPCore

--- phpCore/medMaskCore/entidades/solicitantes/v1/likeInstitucion/index.php
+++ phpCore/medMaskCore/entidades/solicitantes/v1/likeInstitucion/index.php
@@ -7,9 +7,10 @@
 		$sql = $dbConn->prepare("SELECT  s.id, u.id idUsuario, nombre,apellido,pais,provincia,localidad,email,institucion FROM usuarios u JOIN solicitantes s ON u.id=s.idUsuario where institucion like :institucion ");
 		$sql->bindValue(':institucion', "%$_GET[institucion]%");
 		$sql->execute();
+		$sql->setFetchMode(PDO::FETCH_ASSOC);
 		header("HTTP/1.1 200 OK");
 		header('Content-type:application/json');
-		echo json_encode($sql->fetch(PDO::FETCH_ASSOC));
+		echo json_encode($sql->fetchAll());
 		exit();
     }
 ?>
