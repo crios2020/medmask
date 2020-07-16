6fa1d10a650b1c830ffd12d4103141167f79e82d
Carlos <intercrios@hotmail.com>
2020-07-16 18:47:13 -0300

    Se agrego la app web

---
+++ siteWeb/MedMask.js
@@ -0,0 +1,16 @@
+function fu_cambia_pagina(pagina)
+{
+		
+switch (pagina)
+	{
+		case 'login':
+			alert("1");
+			parent.parent.parent.document.getElementById('frame_ppal').src= 'login.html';
+			alert("asda");
+			break;
+		case '2':	
+			break;
+	}
+	
+}
+
